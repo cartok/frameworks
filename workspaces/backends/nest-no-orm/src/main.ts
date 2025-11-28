37881724b572c9def742bf2a70b0c92b13a92c99
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2025-11-28 12:41:07 +0100

    install openapi / swagger

--- workspaces/backends/nest-no-orm/src/main.ts
+++ workspaces/backends/nest-no-orm/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import type { NestFastifyApplication } from "@nestjs/platform-fastify";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "~/app.module";
 import type { Env } from "~/env/env.model";
 import { INJECTION_KEY_ENV } from "~/env/env.module";
@@ -11,6 +12,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true })
   );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,9 +23,20 @@ async function bootstrap() {
       },
     })
   );
-  const env = app.get<Env>(INJECTION_KEY_ENV);
 
   app.setGlobalPrefix("api");
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle("Nest No ORM API")
+    .setDescription("API for the Nest No ORM backend")
+    .setVersion("1.0")
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup("api", app, documentFactory);
+
+  const env = app.get<Env>(INJECTION_KEY_ENV);
   await app.listen(env.API_PORT, "0.0.0.0");
 }
 bootstrap();
